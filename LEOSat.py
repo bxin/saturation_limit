57a4568d594d08808d4734670368a91c45819e5e
Bo Xin <bxin@lsst.org>
2020-07-11 03:37:15 +0000

    add width_vs_mag.ipynb

--- LEOSat.py
+++ LEOSat.py
@@ -26,6 +26,7 @@ def getSatSII(f, d, e, l1, l2, h, zangle, seeing, pixel_size, plate_scale):
     print('donutR = %d micron, satellite conv kernel = %d x %d microns'%(donutR, s1, s2))
     
     padding = 5 #microns
+    print('padding = ', padding)
     side = int(donutR + padding) #this is actually half of side length
     x0=np.linspace(-side,side,side*2+1)
     [x, y] = np.meshgrid(x0,x0)
@@ -52,3 +53,60 @@ def getSatSII(f, d, e, l1, l2, h, zangle, seeing, pixel_size, plate_scale):
     fwhm_exp = np.sqrt(f1+f2+f3)/plate_scale*pixel_size
     print('expected FWHM = %d microns'%(fwhm_exp))
     return sii, fwhm_exp
+
+def getSatSIIvonK(f, d, e, l1, l2, h, zangle, seeing, pixel_size, plate_scale):
+    '''
+    input parameters:
+        f = focal length of telescope in meters
+        d = diameter of telescope primary mirror in meters
+        e = telescope obscuration ratio
+        l1, l2 = apparent size of satellite in meters
+        h = satellite height in meters
+        zangle = zenith angle that the satellite is observed in degrees
+        seeing = seeing (delivered PSF size in arcsec)
+        pixel_size = pixel size in micron
+        plate_scale = plate scale in arcsec per pixel
+    output parameter:
+        sii : satellite instantaneous image, pixel size is 3.5 micron (=0.07 arcsec = 0.35 LSST pixel)
+        fwhm_exp: expected FWHM using FWHM = $\sqrt{\frac{D_{sat}^2}{d^2} + \frac{D_{pupil}^2}{d^2} + \theta_{atm}^2}$, 
+    '''
+    zAtm = np.loadtxt('vonK1.0.txt') #this was generated for 1.0" FWHM, pixel size=0.1". We squeeze it 0.7" FWHM, pixel size=0.07".
+    
+    fno = f/d
+    cosz = np.cos(np.radians(zangle))
+    range = h/cosz
+    donutR=round((1/(1/f-1/range)-f)*1e6/fno/2/3.5) #in 3.5 micron pixel
+    s1 = int(l1/range/np.pi*180*3600/plate_scale*pixel_size/3.5)
+    s2 = int(l2/range/np.pi*180*3600/plate_scale*pixel_size/3.5)
+    print('donutR = %d pixel (1pix = 0.07arcsec), satellite conv kernel = %d x %d microns'%(donutR, s1, s2))
+    
+    padding = np.round((zAtm.shape[0] - s1 - donutR*2)/2) #480 #pixels
+    print('padding = ', padding)
+    side = int(donutR + padding) #this is actually half of side length
+    x0=np.linspace(-side,side,side*2+1)
+    [x, y] = np.meshgrid(x0,x0)
+    r = np.sqrt(x**2+y**2)
+    z = np.zeros((side*2+1, side*2+1))
+    mask = np.bitwise_and((r<donutR), (r>donutR*e))
+    z[mask] = 1 #image of a point object
+    
+    s = np.ones((s1, s2))
+    f = convolve2d(z,s) #sat is a extended source
+    print(z.shape, s.shape, f.shape)
+    
+    #sii = convolve2d(f, zAtm)
+    ffft = np.fft.fftshift(np.fft.fft2(np.fft.fftshift(f), s=zAtm.shape))
+    zfft = np.fft.fftshift(np.fft.fft2(np.fft.fftshift(zAtm), s=zAtm.shape))
+    prodfft = ffft*zfft
+    sii = np.fft.fftshift(np.fft.ifft2(np.fft.fftshift(prodfft),
+                                                    s=prodfft.shape))
+    
+    sii = np.absolute(sii)
+    sii = sii/np.sum(sii)
+    f1 = (max(l1,l2)/range/np.pi*180*3600)**2
+    f2 = (d/range/np.pi*180*3600)**2
+    f3 = seeing**2
+    fwhm_exp = np.sqrt(f1+f2+f3)/plate_scale*pixel_size
+    print('expected FWHM = %d microns'%(fwhm_exp))#this is still in micron
+    return sii, fwhm_exp
+
