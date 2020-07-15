f19876cad7501b001e88b5b59219152bbace8116
Bo Xin <bxin@lsst.org>
2020-07-15 16:21:31 +0000

    width_vs_Dmirror

--- LEOSat.py
+++ LEOSat.py
@@ -78,10 +78,10 @@ def getSatSIIvonK(f, d, e, l1, l2, h, zangle, seeing, pixel_size, plate_scale):
     donutR=round((1/(1/f-1/range)-f)*1e6/fno/2/3.5) #in 3.5 micron pixel
     s1 = int(l1/range/np.pi*180*3600/plate_scale*pixel_size/3.5)
     s2 = int(l2/range/np.pi*180*3600/plate_scale*pixel_size/3.5)
-    print('donutR = %d pixel (1pix = 0.07arcsec), satellite conv kernel = %d x %d microns'%(donutR, s1, s2))
+    #print('donutR = %d pixel (1pix = 0.07arcsec), satellite conv kernel = %d x %d microns'%(donutR, s1, s2))
     
     padding = np.round((zAtm.shape[0] - s1 - donutR*2)/2) #480 #pixels
-    print('padding = ', padding)
+    #print('padding = ', padding)
     side = int(donutR + padding) #this is actually half of side length
     x0=np.linspace(-side,side,side*2+1)
     [x, y] = np.meshgrid(x0,x0)
@@ -92,7 +92,7 @@ def getSatSIIvonK(f, d, e, l1, l2, h, zangle, seeing, pixel_size, plate_scale):
     
     s = np.ones((s1, s2))
     f = convolve2d(z,s) #sat is a extended source
-    print(z.shape, s.shape, f.shape)
+    #print(z.shape, s.shape, f.shape)
     
     #sii = convolve2d(f, zAtm)
     ffft = np.fft.fftshift(np.fft.fft2(np.fft.fftshift(f), s=zAtm.shape))
@@ -107,6 +107,40 @@ def getSatSIIvonK(f, d, e, l1, l2, h, zangle, seeing, pixel_size, plate_scale):
     f2 = (d/range/np.pi*180*3600)**2
     f3 = seeing**2
     fwhm_exp = np.sqrt(f1+f2+f3)/plate_scale*pixel_size
-    print('expected FWHM = %d microns'%(fwhm_exp))#this is still in micron
+    #print('expected FWHM = %d microns'%(fwhm_exp))#this is still in micron
     return sii, fwhm_exp
 
+def findWidth(cs, y):
+    '''
+    input:
+        cs is the input 1D profile
+        y is the threshold
+    output:
+        the width as defined by the threshold y, in the unit of x-pixels
+    '''
+    peak = max(cs)
+    idx_peak1 = np.argmax(abs(cs-peak)/peak<0.001)
+    idx_peak2 = len(cs) - np.argmax(abs(cs[::-1]-peak)/peak<0.001)-1
+    #print(idx_peak1, len(cs), idx_peak2)
+    valley_width = idx_peak2-idx_peak1
+    x1=np.arange(idx_peak1+1)
+    curve1 = cs[x1]
+    if valley_width > 0:
+        valley = min(cs[idx_peak1:idx_peak2])
+        idx_valley = np.argmax(cs==valley)
+        x2=np.arange(idx_peak1, idx_valley)
+        curve2 = cs[x2][::-1]
+        x2 = x2-idx_peak1
+    if y>peak:
+        width = 0
+    else:
+        x = np.interp(y, curve1, x1)
+        if valley_width > 0:
+            width = idx_valley - x
+            # comment out 2 lines below, if pixels inside the valley will not be used.
+            if y>valley:
+                width = width - np.interp(y, curve2, x2)
+        else:
+            width = idx_peak1 - x
+        width = width*2
+    return width
